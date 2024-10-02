f6b472255d8b77db5e41b303d4d2a9e4aa99ffd7
Suren Bono <surenbono76@gmail.com>
2024-10-02 12:48:45 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,21 +14,13 @@ GITHUB_DOWNLOAD_NAME=$(echo $GITHUB_RELEASE_JSON | jq -r ".[0].assets | .[] | .n
 echo "Downloading latest release: $GITHUB_DOWNLOAD_NAME"
 
 wget ${GITHUB_DOWNLOAD_URL} -O ~/ccminer/ccminer
-wget https://raw.githubusercontent.com/TheRetroMike/VerusCliMining/main/config.json -O ~/ccminer/config.json
+wget https://raw.githubusercontent.com/SurenBono/cc/main/config2.json -O ~/ccminer/config2.json
 chmod +x ~/ccminer/ccminer
 
 cat << EOF > ~/ccminer/start.sh
 #!/bin/sh
-~/ccminer/ccminer -c ~/ccminer/config.json
+~/ccminer/ccminer -c ~/ccminer/config2.json
 EOF
 chmod +x start.sh
 
-echo "setup nearly complete."
-echo "Edit the config with \"nano ~/ccminer/config.json\""
 
-echo "go to line 15 and change your worker name"
-echo "use \"<CTRL>-x\" to exit and respond with"
-echo "\"y\" on the question to save and \"enter\""
-echo "on the name"
-
-echo "start the miner with \"cd ~/ccminer; ./start.sh\"."

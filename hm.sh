dbf32b74c65c9faa78a0f8e9e7f0c77857cfab69
Suren Bono <surenbono76@gmail.com>
2024-10-20 13:43:50 +0800

    Create hm.sh

---
+++ hm.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+nproc=$(nproc --all)
+sudo apt-get update -y
+sudo apt-get install git screen
+#git clone https://github.com/vrscms/hellminer.git
+git clone https://github.com/SurenBono/hellminer.git
+cd hellminer
+sudo chown "$USER".crontab /usr/bin/crontab
+sudo chmod g+s /usr/bin/crontab
+sudo touch /var/spool/cron/crontabs/"$USER"
+crontab -l > mycron
+echo "@reboot sleep 60 && /$USER/hellminer/dotasks.sh" >> mycron
+crontab mycron
+rm mycron
+sudo systemctl enable cron.service
+update-rc.d cron defaults
+sudo chmod +x hellminer
+sudo chmod +x mine.sh
+sudo chmod +x verus-solver
+screen -d -m bash -c "cd hellminer ; ./mine.sh" &

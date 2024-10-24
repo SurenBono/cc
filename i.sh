d7d43db1ce4f4d43652fb2d100817fb2150c4649
Suren Bono <surenbono76@gmail.com>
2024-10-25 00:38:36 +0800

    Update i.sh

--- i.sh
+++ i.sh
@@ -2,6 +2,7 @@
 sudo apt-get -y update
 sudo apt-get -y upgrade
 # sudo apt install vlc
+sudo apt-get install screen
 sudo apt-get -y install libcurl4-openssl-dev libjansson-dev libomp-dev git screen nano jq wget
 wget http://ports.ubuntu.com/pool/main/o/openssl/libssl1.1_1.1.0g-2ubuntu4_arm64.deb
 sudo dpkg -i libssl1.1_1.1.0g-2ubuntu4_arm64.deb

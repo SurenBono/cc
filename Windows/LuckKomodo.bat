9393fd23cdd331a5946fe1ee0d1239733e92c753
Suren Bono <surenbono76@gmail.com>
2025-02-26 07:42:42 +0800

    Update LuckKomodo.bat

--- Windows/LuckKomodo.bat
+++ Windows/LuckKomodo.bat
@@ -1,3 +1,3 @@
 ccminer.exe -a verus -o
-stratum+tcp://ap.luckpool.net:3956-u RDLQZKbHMjSkhyQUG6u1AD5D8Lhqn6DmzB.W1 -p x -t 5
+stratum+tcp://ap.luckpool.net:3956 -u RDLQZKbHMjSkhyQUG6u1AD5D8Lhqn6DmzB.W1 -p x -t 5
 pause

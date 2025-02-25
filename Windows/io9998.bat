7587aa9640b505d5fcb5447ecf62f9b61c4a65cc
Suren Bono <surenbono76@gmail.com>
2025-02-26 07:44:16 +0800

    Update io9998.bat

--- Windows/io9998.bat
+++ Windows/io9998.bat
@@ -1,2 +1,5 @@
-ccminer.exe -a verus -o stratum+tcp://pool.veruscoin.io:9998 -u R9QnyT5j4515RZnwg6T8KYAp7EskoWgfGT.W1 -p x -t 5
+ccminer.exe -a verus 
+-o stratum+tcp://pool.veruscoin.io:9998 
+-u R9QnyT5j4515RZnwg6T8KYAp7EskoWgfGT.W1 
+-p x -t 5
 pause

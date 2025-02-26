daa41cfc2076b4d2adca18e2da2c5cbf25dc67b1
Suren Bono <surenbono76@gmail.com>
2025-02-26 08:01:35 +0800

    Update luckRadd.bat

--- Windows/luckRadd.bat
+++ Windows/luckRadd.bat
@@ -1,5 +1,2 @@
-ccminer.exe -a verus 
--o stratum+tcp://ap.luckpool.net:3956
--u R9QnyT5j4515RZnwg6T8KYAp7EskoWgfGT.W1 
--p x -t 5
+ccminer.exe -a verus -o stratum+tcp://ap.luckpool.net:3956 -u R9QnyT5j4515RZnwg6T8KYAp7EskoWgfGT.W1 -p x -t 5
 pause
